001cd6bd40d4ec07fe0020f07c86aca4b6e4d7c1
Derek Riter <technoturtle7d@gmail.com>
2024-04-12 23:29:50 -0400

    Basic triangle drawing

--- src/draw.h
+++ src/draw.h
@@ -18,8 +18,8 @@ void gfxInit() {
     
     gfx_palette[COL_WHITE] = gfx_RGBTo1555(255, 255, 255);
     gfx_palette[COL_BLACK] = gfx_RGBTo1555(0, 0, 0);
-    gfx_palette[COL_GREEN] = gfx_RGBTo1555(0, 255, 0);
-    gfx_palette[COL_RED] = gfx_RGBTo1555(255, 0, 0);
+    gfx_palette[COL_GREEN] = gfx_RGBTo1555(0, 200, 0);
+    gfx_palette[COL_RED] = gfx_RGBTo1555(200, 0, 0);
     
     gfx_SetTransparentColor(COL_WHITE);
     gfx_SetTextFGColor(COL_BLACK);
@@ -28,7 +28,31 @@ void gfxInit() {
 void gfxEnd() {
     gfx_End();
 }
-void gfxRender() {
+
+void gfxDrawTri(Tri tri) {
+    const Vec2 windowSize = {GFX_LCD_WIDTH - 40, GFX_LCD_HEIGHT - 60};
+    const Vec2 windowPos = {20, 30};
+    
+    float scale = 30;
+    if(tri.sA * scale > windowSize.y) scale = windowSize.y / tri.sA;
+    if(tri.sB * scale > windowSize.x) scale = windowSize.x / tri.sB;
+    
+    Vec2 p1 = {(uint24_t) ((-tri.sB / 2.0f) * scale + windowSize.x / 2.0f + windowPos.x), (uint24_t) ((tri.sA / 2.0f) * scale + windowSize.y / 2.0f + windowPos.y)};
+    Vec2 p2 = {(uint24_t) ((-tri.sB / 2.0f) * scale + windowSize.x / 2.0f + windowPos.x), (uint24_t) ((-tri.sA / 2.0f) * scale + windowSize.y / 2.0f + windowPos.y)};
+    Vec2 p3 = {(uint24_t) ((tri.sB / 2.0f) * scale + windowSize.x / 2.0f + windowPos.x), (uint24_t) ((tri.sA / 2.0f) * scale + windowSize.y / 2.0f + windowPos.y)};
+    
+    gfx_SetColor(COL_BLACK);
+    gfx_HorizLine_NoClip(p1.x, (uint24_t) (p1.y - 0.33 * scale), (uint24_t) (0.33 * scale));
+    gfx_VertLine_NoClip((uint24_t) (p1.x + 0.33 * scale), (uint24_t) (p1.y - 0.33 * scale), (uint24_t) (0.33 * scale));
+    
+    gfx_SetColor(COL_GREEN);
+    gfx_Line_NoClip(p1.x, p1.y, p2.x, p2.y);
+    gfx_Line_NoClip(p1.x, p1.y, p3.x, p3.y);
+    gfx_Line_NoClip(p2.x, p2.y, p3.x, p3.y);
+    
+    return;
+}
+void gfxRender(RenderInfo info) {
     gfx_FillScreen(COL_WHITE);
     
     gfx_SetTextFGColor(COL_BLACK);
@@ -37,9 +61,11 @@ void gfxRender() {
     gfx_PrintStringXY(VERSION, GFX_LCD_WIDTH - width - 1, GFX_LCD_HEIGHT - CHAR_HEIGHT - 1);
     
     //controls
+    gfx_PrintStringXY("[ARROWS] - Adjust", 1, GFX_LCD_HEIGHT - CHAR_HEIGHT * 2 - 2);
     gfx_PrintStringXY("[del] - Exit", 1, GFX_LCD_HEIGHT - CHAR_HEIGHT - 1);
     
-    gfx_SwapDraw();
+    gfxDrawTri(info.tri);
     
+    gfx_SwapDraw();
     needsRedraw = false;
 }
--- src/main.c
+++ src/main.c
@@ -2,18 +2,59 @@
 #define uint unsigned int
 
 #include <keypadc.h>
+#include <math.h>
+
+typedef struct Vec2 {
+    int x, y;
+} Vec2;
+typedef struct Tri {
+    float sA, sB, sC, a1, a2;
+} Tri;
+typedef struct RenderInfo {
+    Tri tri;
+} RenderInfo;
 
 #include "draw.h"
 
 int main() {
     gfxInit();
     
+    Tri tri = {1, 1, sqrtf(2), 60, 60}; //default isos rt tri
+    
+    bool upLast = false;
+    bool downLast = false;
+    bool leftLast = false;
+    bool rightLast = false;
     while(1) {
         kb_Scan();
         
         if(kb_IsDown(kb_KeyDel)) break;
+        else if(kb_IsDown(kb_KeyUp) && !upLast) {
+            tri.sA++;
+            needsRedraw = true;
+        }
+        else if(kb_IsDown(kb_KeyDown) && !downLast && tri.sA > 1) {
+            tri.sA--;
+            needsRedraw = true;
+        }
+        else if(kb_IsDown(kb_KeyLeft) && !leftLast && tri.sB > 1) {
+            tri.sB--;
+            needsRedraw = true;
+        }
+        else if(kb_IsDown(kb_KeyRight) && !rightLast) {
+            tri.sB++;
+            needsRedraw = true;
+        }
         
-        if(needsRedraw) gfxRender();
+        if(needsRedraw) {
+            RenderInfo info = {tri};
+            gfxRender(info);
+        }
+        
+        upLast = kb_IsDown(kb_KeyUp);
+        downLast = kb_IsDown(kb_KeyDown);
+        leftLast = kb_IsDown(kb_KeyLeft);
+        rightLast = kb_IsDown(kb_KeyRight);
     }
     
     gfxEnd();
